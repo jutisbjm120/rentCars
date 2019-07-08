c7e22d625f529b00059ebd10989411784713dffb
shishishi8888 <1186324052@qq.com>
2019-07-08 11:56:18 +0800

    空项目文档

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    主页
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
